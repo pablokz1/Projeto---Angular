1ade3ddb632546cdd77e9ce3554cc9b5fc301f89
pablokz1 <pablo.carvalho16@hotmail.com>
2023-05-12 11:11:43 -0300

    ajustes

--- crud/frontend/src/app/app-routing.module.ts
+++ crud/frontend/src/app/app-routing.module.ts
@@ -1,15 +1,12 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './views/home/home.component'
 import { ProductCrudComponent } from './views/product-crud/product-crud.component'
 
-const routes: Routes = [{
-  path: "",
-  component: HomeComponent
-}, {
-  path: "products",
-  component: ProductCrudComponent
+const routes: Routes = [
+  {path: "", component: HomeComponent}, 
+  {path: "produtos", component: ProductCrudComponent
 }];
 
 @NgModule({
